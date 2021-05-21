20525d05c1044f73638d604ad60f13c4cb9bb283
Eric Souza <eric.bh18souza@gmail.com>
2021-05-21 14:18:12 -0300

    Main sections created and clip effect added to header

---
+++ src/data/index.ts
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: 'Focus only on important work',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+  },
+
+  {
+    title: 'Say goodbye to tiring work',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+  },
+
+  {
+    title: 'File storage and sharing are only the beginning',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis.',
+  },
+
+  {
+    title: 'For all',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  
+  {
+    title: 'Footer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis.',
+  },
+];
