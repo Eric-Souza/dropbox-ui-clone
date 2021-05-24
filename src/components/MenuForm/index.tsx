4aec3b251a0786cf7e6a542cc92730fb13aac473
Eric Souza <eric.bh18souza@gmail.com>
2021-05-24 10:23:10 -0300

    Form menu styling complete

--- src/components/MenuForm/index.tsx
+++ src/components/MenuForm/index.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Navigation, DropboxLogo, Form } from './styles';
 
 const MenuForm: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Navigation>
+        <h1>
+          <DropboxLogo />
+
+          <span>Dropbox</span>
+        </h1>
+
+        <button className='action--close'>✕</button>
+      </Navigation>
+
+      <Form>
+        <span className='title'>
+          Log in
+        </span>
+
+        <span className='subtitle'>
+          Please fill the form below
+        </span>
+
+        <input type="text" placeholder="Name"></input>
+        <input type="text" placeholder="Surname"></input>
+        <input type="email" placeholder="Email"></input>
+        <input type="password" placeholder="Password"></input>
+
+        <button>
+          Log in
+        </button>
+
+        <span className='terms'>
+          Terms and conditions
+        </span>
+      </Form>
+    </Container>
+  );
 }
 
 export default MenuForm;
